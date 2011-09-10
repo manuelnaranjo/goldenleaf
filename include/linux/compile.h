8a42a879518c84fdd861072c1696c2936d7b6564
Gérson Barreiros <fserve@gmail.com>
2011-09-10 13:28:34 -0400

    Revert "HAVE_CLK=y as default"
    This reverts commit 678a56b0676bba003c60a5ea3d1bad908d8dc01a.
    
    Conflicts:
    
    	config_battery_freak

--- include/linux/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 8 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 PREEMPT Fri Sep 9 03:24:49 AMT 2011"
-#define LINUX_COMPILE_TIME "03:24:49"
-#define LINUX_COMPILE_BY "lg-electronics"
-#define LINUX_COMPILE_HOST "goldenleaf"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
